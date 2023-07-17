deb7b0b353f77c83ab5c71e5b478423f25111360
marek.hirschner <hirschner.marek@gmail.com>
2023-07-17 08:59:12 +0200

    Restricted tooltip only to Login and Registration

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -11,13 +11,13 @@ import {computed} from "vue";
 import ChangeLanguage from "@/components/ChangeLanguage.vue";
 import DarkModeSwitch from "@/components/DarkModeSwitch.vue";
 import PortfolioTooltip from "@/components/PortfolioTooltip.vue";
+import {useRoute} from "vue-router";
 
-const location = computed(() => {
-    return 'Login';
+const route = useRoute();
 
-});
 const portfolio = computed(() => {
-    return import.meta.env.VITE_PORTFOLIO === 'true' || false;
+    return (import.meta.env.VITE_PORTFOLIO === 'true' &&
+        (route.name === 'Login' || route.name === 'Registration')) || false;
 })
 </script>
 
